70e80f0bbcf317b7d071418d507d901dd014f879
sheep <319788742@qq.com>
2024-08-14 01:23:20 +0800

    5

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
-import router from './router'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+import router from "./router/index.js"
 
 const app = createApp(App)
-
-app.use(router).use(ElementPlus).mount('#app')
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+app.use(router)
+app.use(ElementPlus)
+app.mount('#app')
