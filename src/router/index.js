c5ec40fc18a66e03e42ef26ee509d3ff70612c00
sheep <319788742@qq.com>
2024-08-15 06:10:55 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from "vue-router";
 const routes = [
     {
         path:'/',
-        redirect:'menu'
+        redirect:'login'
     },
     {
         path :'/login',
