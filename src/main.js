53c9c98d9d7746655c930c670c72ea475a249aba
sheep <319788742@qq.com>
2024-08-15 05:03:58 +0800

    1

--- src/main.js
+++ src/main.js
@@ -7,10 +7,15 @@ import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 import router from "./router/index.js"
 
+import BaiduMap from 'vue-baidu-map-3x'
+
 const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
+app.use(BaiduMap, {
+  ak: 'ERJaT2zREz57EesvlgMEX0fp7aMP1p8k',
+});
 app.use(router)
 app.use(ElementPlus)
 app.mount('#app')
--- src/views/Systemlogs/index.vue
+++ src/views/Systemlogs/index.vue
@@ -1,4 +1,81 @@
-<template><div>Systemlogs</div></template>
-<script setup></script>
+<template>
+  <div class="Systmlogs">
+    <el-container>
+      <el-header>系统控制</el-header>
+      <el-container>
+        <el-main>
+          <baidu-map
+            class="map"
+            :zoom="16"
+            :center="{ lng: 114.399724, lat: 30.525502 }"
+            :scroll-wheel-zoom="true"
+            ><bm-geolocation
+              anchor="BMAP_ANCHOR_BOTTOM_RIGHT"
+              :showAddressBar="true"
+              :autoLocation="true"
+            ></bm-geolocation>
+            <bm-scale anchor="BMAP_ANCHOR_TOP_RIGHT"></bm-scale>
+            <bm-copyright
+              anchor="BMAP_ANCHOR_TOP_RIGHT"
+              :copyright="[
+                {
+                  id: 1,
+                  content: 'Copyright Message',
+                  bounds: { ne: { lng: 110, lat: 40 }, sw: { lng: 0, lat: 0 } },
+                },
+                { id: 2, content: '<a>永远的神 版权所有</a>' },
+              ]"
+            >
+            </bm-copyright>
+            <!-- 点击一个点就有一个弹框 -->
+            <bm-marker
+              :position="{ lng: 114.400746, lat: 30.527161 }"
+              @click="infoWindowOpen"
+            >
+              <bm-info-window
+                :show="show"
+                @close="infoWindowClose"
+                @open="infoWindowOpen"
+                >我爱武汉</bm-info-window
+              >
+            </bm-marker>
+          </baidu-map>
+        </el-main>
+        <el-aside width="200px">Aside</el-aside>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+<script setup>
+import { ref } from "vue";
+const show = ref(false);
+const infoWindowClose = () => {
+  show.value = false;
+};
+const infoWindowOpen = () => {
+  show.value = true;
+};
+</script>
 
-<style scoped></style>
+<style scoped>
+.el-header {
+  display: flex;
+  height: 55px;
+  background: #b3c0d1;
+  color: #333;
+  align-items: center;
+}
+.el-main {
+  height: 669.333px;
+  padding: 1 2px;
+}
+.map {
+  width: 100%;
+  height: 100%;
+}
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  height: 669.333px;
+}
+</style>
